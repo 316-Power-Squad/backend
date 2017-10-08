fa7747227d506be4102b0fa0f831e9275dafd7cc
Zachary Marion <zfm2@duke.edu>
2017-10-07 23:01:23 -0400

    Grab name and state

--- helpers/scraper.js
+++ helpers/scraper.js
@@ -1,29 +1,45 @@
-var request = require('request'), cheerio = require('cheerio'), urls = [];
-
+var request = require('request'),
+  cheerio = require('cheerio'),
+  urls = [];
 
 var notTitle = function(s) {
-	if (s == "Primary Conference" || s == "Making Transition" || s == "Full Membership" || s == "Future Conference") {
-		return false;
-	} if (s == "Savannah State University" || s == "California Baptist University" || s == "North Alabama !University of North Alabama") {
-		return false;
-	}
-	return true;
-}
+  if (
+    s == 'Primary Conference' ||
+    s == 'Making Transition' ||
+    s == 'Full Membership' ||
+    s == 'Future Conference'
+  ) {
+    return false;
+  }
+  if (
+    s == 'Savannah State University' ||
+    s == 'California Baptist University' ||
+    s == 'North Alabama !University of North Alabama'
+  ) {
+    return false;
+  }
+  return true;
+};
 
-request('https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_institutions', function(err, resp, body) {
-	if (!err && resp.statusCode == 200) {
-		console.log('cake');
-		var $ = cheerio.load(body);
-		var count = 0;
-		$('th', '.sortable').each(function() {
-			var text = $(this).text();
-			if (text.split(" ").length > 1 && notTitle(text)) {
-				console.log(text);
-				count++;
-			}
-		});
-	}
-	else {
-		console.log(resp.statusCode);
-	}
-});
+request(
+  'https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_institutions',
+  function(err, resp, body) {
+    if (!err && resp.statusCode == 200) {
+      console.log('cake');
+      var $ = cheerio.load(body);
+      var count = 0;
+      $('tr', '.sortable').each(function() {
+        const row = $(this)
+          .text()
+          .split('\n');
+        const data = [row[1], row[4]];
+        console.log(data);
+        if (data[0].split(' ').length > 1 && notTitle(data[0])) {
+          count++;
+        }
+      });
+    } else {
+      console.log(resp.statusCode);
+    }
+  }
+);
