7c06fd834277c5c4650824ee752957f7dc44eda1
jmb145 <jmb145@duke.edu>
2017-10-08 18:53:10 -0400

    even more formatting

--- helpers/scraper.js
+++ helpers/scraper.js
@@ -85,11 +85,60 @@ var getAbbrev = function(state) {
     return "";
 }
 
+var getCalifornia = function(school) {
+  let splitSchool = school.split(" ");
+  let usableWords = [];
+  for (var i = 0; i < splitSchool.length; i++) {
+      if (splitSchool[i].includes("!")) {
+        break;
+      } else {
+        usableWords.push(splitSchool[i]);
+      }
+    } 
+    if (school.includes("Los Angeles")) {
+      return "UCLA";
+    }
+  if (school.includes("Berkeley")) {
+    return "California";
+  } if (school.includes("University of California")) {
+      usableWords[0] = "UC";
+      return usableWords.join("_");
+    }
+   if (school.includes("California State University")) {
+      if (
+        splitSchool[splitSchool.length - 1] == "Fresno" || 
+        splitSchool[splitSchool.length - 1] == "Sacramento" || 
+        splitSchool[splitSchool.length - 1] == "Long Beach"
+        ) {
+        return splitSchool[splitSchool.length - 1].concat(" State");
+      } else {
+        return "Cal_St_".concat(splitSchool[splitSchool.length - 1]);
+      }
+  }
+}
+
+var getNC = function (school) {
+    let splitSchool = school.split(" ");
+    if (school.includes("Chapel Hill")) {
+      return "North Carolina";
+    } else {
+        return "UNC_".concat(splitSchool[2]);
+    }
+}
+
 var getTeamName = function(school) {
   let usableWords = [];
-  var splitSchool = school.split(" ");
-  if (school == "Boston Universty") {
-    return "Boston_U"
+  let splitSchool = school.split(" ");
+  if (school == "Boston University") {
+      return "Boston_U";
+  } if (school.includes("Brigham Young")) {
+      return "BYU";
+  } if (school.includes("California State University") || school.includes("University of California")) {
+      return getCalifornia(school);
+  } if (school.includes("California Polytechnic")) {
+      return "Cal_Poly";
+  } if (school.includes("University of North Carolina")) {
+      return getNC(school);
   }
   for (var i = 0; i < splitSchool.length; i++) {
       if (splitSchool[i].includes("!")) {
@@ -98,38 +147,36 @@ var getTeamName = function(school) {
         usableWords.push(splitSchool[i]);
       }
     } 
-  if (splitSchool[splitSchool.length - 1] == "University") {
-    if (splitSchool.length > 2) {
-      if (splitSchool[splitSchool.length - 2] == "State") {
-        splitSchool[splitSchool.length - 2] = "St"
-      }
-    }
-    splitSchool[splitSchool.length - 1] = "";
-    splitSchool = splitSchool.splice(0, splitSchool.length - 1);
-    return splitSchool.join("_")
+  if (usableWords[usableWords.length - 1] == "University") {
+    usableWords[usableWords.length - 1] = "";
+    usableWords = usableWords.splice(0, usableWords.length - 1);
+    return usableWords.join("_")
   }
   return school;
 }
-
-request(
-  'https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_institutions',
-  function(err, resp, body) {
-    if (!err && resp.statusCode == 200) {
-      console.log('cake');
-      var $ = cheerio.load(body);
-      var count = 0;
-      $('tr', '.sortable').each(function() {
-        const row = $(this)
-          .text()
-          .split('\n');
-        const data = [getTeamName(row[1]), getAbbrev(row[4])];
-        console.log(data);
-        if (data[0].split(' ').length > 1 && notTitle(data[0])) {
-          count++;
+var getSchoolNames = function() {
+    request(
+      'https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_institutions',
+      function(err, resp, body) {
+        if (!err && resp.statusCode == 200) {
+          console.log('cake');
+          var $ = cheerio.load(body);
+          var count = 0;
+          $('tr', '.sortable').each(function() {
+            const row = $(this)
+              .text()
+              .split('\n');
+            const data = [getTeamName(row[1]), getAbbrev(row[4])];
+            console.log(data);
+            if (data[0].split(' ').length > 1 && notTitle(data[0])) {
+              count++;
+            }
+          });
+        } else {
+          console.log(resp.statusCode);
         }
-      });
-    } else {
-      console.log(resp.statusCode);
-    }
-  }
-);
+      }
+    );
+}
+
+getSchoolNames();
