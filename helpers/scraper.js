2aa1b12cff5d9b72c21258c89818ff272cff2663
jmb145 <jmb145@duke.edu>
2017-10-07 22:24:32 -0400

    Added the first iteration of the scraper.

---
+++ helpers/scraper.js
@@ -0,0 +1,29 @@
+var request = require('request'), cheerio = require('cheerio'), urls = [];
+
+
+var notTitle = function(s) {
+	if (s == "Primary Conference" || s == "Making Transition" || s == "Full Membership" || s == "Future Conference") {
+		return false;
+	} if (s == "Savannah State University" || s == "California Baptist University" || s == "North Alabama !University of North Alabama") {
+		return false;
+	}
+	return true;
+}
+
+request('https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_institutions', function(err, resp, body) {
+	if (!err && resp.statusCode == 200) {
+		console.log('cake');
+		var $ = cheerio.load(body);
+		var count = 0;
+		$('th', '.sortable').each(function() {
+			var text = $(this).text();
+			if (text.split(" ").length > 1 && notTitle(text)) {
+				console.log(text);
+				count++;
+			}
+		});
+	}
+	else {
+		console.log(resp.statusCode);
+	}
+});
