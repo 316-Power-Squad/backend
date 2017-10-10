425f4c010cb4111fd5d1333af701cee1bc3120de
Zachary Marion <zfm2@duke.edu>
2017-10-10 18:00:32 -0400

    Add queries

--- first_milestone/queries.sql
+++ first_milestone/queries.sql
@@ -1,1 +1,41 @@
 -- This file contains the queries that will represent the API for the site
+
+-- Getting a user's password hash to check that it matches. On the server
+-- we then pass back a JWT token to the frontend so that their subsequent
+-- calls to the API are authenticated
+SELECT hash
+FROM User
+WHERE email='test@example.com';
+
+-- Get all teams and their rank in a given region (in this case the northeast)
+SELECT team_name, team_rank
+FROM TeamWithRegion
+WHERE region='northeast';
+
+-- Get all meets that a team participated in (in this case louiville)
+SELECT Meet.name
+FROM Meet, Participates
+WHERE Meet.id = Participates.meet_id AND 
+  Participates.team_id = (
+    SELECT id FROM Team WHERE Team.name='louisville'
+  );
+
+-- Getting all the teams that competed in a given meet for a given
+-- gender (in this case men's battle in beantown)
+SELECT Team.name AS team_name, placement
+FROM Team, Meet, Participates
+WHERE Meet.name='battle_in_beantown' AND
+  Team.gender='mens' AND
+  Team.id = Participates.team_id AND
+  Meet.id = Participates.meet_id
+ORDER BY Participates.placement;
+
+-- Getting the top 2 teams from each region (we assume that these are the
+-- two teams that are getting automatic bids to nationals)
+SELECT t1.team_name, t1.region, t1.team_rank  
+FROM TeamWithRegion AS t1 JOIN TeamWithRegion AS t2
+  ON t1.region = t2.region AND 
+    t1.team_rank < t2.team_rank
+  GROUP BY t1.team_name, t1.region, t1.team_rank
+  HAVING COUNT(*) <= 2
+  ORDER BY t1.region, t1.team_rank;
--- first_milestone/seed.sql
+++ first_milestone/seed.sql
@@ -4,7 +4,8 @@
 --     mysql -u root -p atlargetest < seed.sql
 
 -- First we make sure our database exists and drop any existing tables.
-DROP TABLE IF EXISTS Team, User, Admin, Meet, Participates, RegionalRank;
+DROP TABLE IF EXISTS Team, Region, TeamInRegion, User, Admin, Meet, Participates;
+DROP VIEW IF EXISTS TeamWithRegion;
 
 -- User's are useful in web applications for handling API permissions, and 
 -- in the future we may want to add the ability to associate certain data
@@ -15,43 +16,103 @@ CREATE TABLE User (
   hash varchar(255) NOT NULL,
   PRIMARY KEY (email)
 );
--- Notion of an admin that can make certain queries (adding / removing 
--- teams, etc) that a regular user cannot
-CREATE TABLE Admin (
-  email varchar(255) NOT NULL REFERENCES User(email),
-  PRIMARY KEY (email)
-);
 -- Description of Team
 CREATE TABLE Team (
   ID int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  gender ENUM('mens', 'womens') NOT NULL,
+  PRIMARY KEY (ID)
+);
+-- Description of TeamInRegion
+CREATE TABLE TeamInRegion (
+  team_id int NOT NULL REFERENCES Team(id),
+  region_id int NOT NULL REFERENCES Region(id),
+  team_rank int NOT NULL,
+  PRIMARY KEY (team_id, region_id)
+);
+-- Description of Region
+CREATE TABLE Region (
+  ID int NOT NULL AUTO_INCREMENT,
   name varchar(255),
-  gender ENUM('mens', 'womens'),
-  region varchar(255),
   PRIMARY KEY (ID)
 );
 -- Description of Meet
 CREATE TABLE Meet (
   ID int NOT NULL AUTO_INCREMENT,
   name varchar(255),
-  date datetime,
+  date date,
   PRIMARY KEY (ID)
 );
 -- Description of Participates
 CREATE TABLE Participates (
   team_id int NOT NULL REFERENCES Team(id),
   meet_id int NOT NULL REFERENCES Meet(id),
-  placement int,
+  placement int NOT NULL,
   PRIMARY KEY (team_id, meet_id)
 );
--- Description of Participates
-CREATE TABLE RegionalRank (
-  team_id int NOT NULL REFERENCES Team(id),
-  rank int,
-  PRIMARY KEY (team_id)
+
+-- VIEWS
+CREATE VIEW TeamWithRegion AS (
+  SELECT Team.name AS team_name, Team.gender, TeamInRegion.team_rank, Region.name AS region 
+  FROM Team, TeamInRegion, Region 
+  WHERE Team.id = TeamInRegion.team_id AND TeamInRegion.region_id = Region.id
 );
 
---------------------------------------------------------------
------------------------- SAMPLE DATA -------------------------
---------------------------------------------------------------
-INSERT INTO Team (name, gender, region) VALUES 
-  ('Duke', 'mens', 'Southern');
+-- SAMPLE DATA 
+INSERT INTO User (email, name, hash) VALUES 
+  ('test@example.com', 'Test User', 'hash1'),
+  ('test2@example.com', 'Test2 User', 'hash2');
+
+INSERT INTO Region (name) VALUES
+  ('northeast'),
+  ('midatlantic'),
+  ('southeast'),
+  ('south'),
+  ('southcentral'),
+  ('greatlakes'),
+  ('midwest'),
+  ('mountain'),
+  ('west');
+
+INSERT INTO Team (name, gender) VALUES 
+  ('syracuse', 'mens'),
+  ('iona', 'mens'),
+  ('cornell', 'mens'),
+  ('georgetown', 'mens'),
+  ('penn', 'mens'),
+  ('villanova', 'mens'),
+  ('louisville', 'mens'),
+  ('virginia', 'mens'),
+  ('eastern kentucky', 'mens');
+
+INSERT INTO TeamInRegion (team_id, region_id, team_rank) VALUES
+  (1, 1, 1),
+  (2, 1, 2),
+  (3, 1, 3),
+  (4, 2, 1),
+  (5, 2, 2),
+  (6, 2, 3),
+  (7, 3, 1),
+  (8, 3, 2),
+  (9, 3, 3);
+
+INSERT INTO Meet (name, date) VALUES
+  ('battle_in_beantown', '2017-09-10'),
+  ('panorama_farms', '2017-09-18'),
+  ('princeton_invitational', '2017-09-30'),
+  ('paul_short', '2017-10-10');
+
+INSERT INTO Participates (team_id, meet_id, placement) VALUES
+  (1, 1, 1),
+  (2, 1, 2),
+  (3, 1, 3),
+  (4, 2, 3),
+  (5, 2, 2),
+  (6, 2, 1),
+  (6, 3, 1),
+  (2, 3, 2),
+  (9, 3, 3),
+  (7, 3, 4),
+  (7, 4, 1),
+  (8, 4, 3),
+  (9, 4, 2);
