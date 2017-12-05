181418a531de2790cf826c108a52f25e909df2ed
Zachary Marion <zfm2@duke.edu>
2017-12-05 14:55:38 -0500

    Added kolas algo

--- routers/api/Rankings.js
+++ routers/api/Rankings.js
@@ -2,16 +2,14 @@ import express from 'express';
 const router = express.Router();
 import Ranking from '../../models/Ranking';
 import { formatResponse } from '../../helpers/api';
+import algorithm from '../../kolas_algorithm';
 
 router.get('/', async (req, res) => {
   try {
     // const teams = await Ranking.getRegionals('mens');
-    const meets = await Ranking.getMeets('mens');
-    res.send(
-      formatResponse({
-        meets,
-      })
-    );
+    // const meets = await Ranking.getMeets('mens');
+    const result = await algorithm();
+    res.send(formatResponse(result));
   } catch (err) {
     res.send(formatResponse({}, err));
   }
