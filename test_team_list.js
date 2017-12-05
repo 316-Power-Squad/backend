a65077c002f3badba422af6eb1bef746ec944d95
npulimood <np96@duke.edu>
2017-12-05 12:40:21 -0500

    check test_team_list.js

--- test_team_list.js
+++ test_team_list.js
@@ -2,26 +2,34 @@ import request from 'request';
 import db, { MODE_TEST } from './helpers/db';
 
 export const getTeam = (id) => {
-  db.get().query(`SELECT * FROM Meet`, (err, rows) => {
-    console.log(err);
-    console.log(rows);
-  });
+  return new Promise((resolve, reject) => {
+    db.get().query(`SELECT * FROM Meet`, (err, rows) => {
+    		if (err) {
+          console.log(err);
+          reject({
+    				code: 'some_unique_code',
+    				message: 'some error message'
+    			});
+    		}
+        resolve(rows);
+    });
+  }
 };
 
-export default async () => {
-	return new Promise((resolve, reject) => {
-    console.log("Starting");
-		insertTeam("df");
-		if (err) {
-      console.log(err);
-			reject({
-				code: 'some_unique_code',
-				message: 'some error message'
-			});
-		}
-		resolve([ /* Array of teams */ ]);
-	})
-}
+// export default async () => {
+// 	return new Promise((resolve, reject) => {
+//     console.log("Starting");
+// 		insertTeam("df");
+// 		if (err) {
+//       console.log(err);
+// 			reject({
+// 				code: 'some_unique_code',
+// 				message: 'some error message'
+// 			});
+// 		}
+// 		resolve([ /* Array of teams */ ]);
+// 	})
+// }
 
 db.connect(MODE_TEST, async () => {
   await getTeam("Duke");
